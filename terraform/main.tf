b194889b20a5d9ffb8050bbec55cf422ff1f43ab
ltadmin <ltomaszewski@gmail.com>
2022-01-17 22:52:23 +0100

    Add new config

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,12 @@
 provider "aws" {
+access_key = ${{ secrets.AWS_ACCESS-KEY_ID }}
+secret_key = ${{ secrets.AWS_SECRET_ACCESS-KEY }}
 region = "eu-central-1"
 
 }
 
 resource "aws_s3_bucket" "s3bucket" {
-bucket = "Web_site"
+bucket = "Web_sites"
 acl = "public-read"
 
 
