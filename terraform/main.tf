9617e9d4123b96d8c5d19df4186dccb8afccf216
ltomaszewski-tech <97832956+ltomaszewski-tech@users.noreply.github.com>
2022-01-17 23:06:28 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
 access_key = "${{ secrets.AWS_ACCESS_KEY_ID }}"
-secret_key = "${{ secrets.AWS_SECRET_ACCESS-KEY }}"
+secret_key = "${{ secrets.AWS_SECRET_ACCESS_KEY }}"
 region = "eu-central-1"
 
 }
